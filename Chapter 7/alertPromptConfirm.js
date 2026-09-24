930eeacc6fedc12d22cf7206903cec134198520d
nontogumbi94 <nprominant@gmail.com>
2026-09-24 14:55:26 +0200

    Add files via upload

---
+++ Chapter 7/alertPromptConfirm.js
@@ -0,0 +1,15 @@
+// Creating a prompt
+var name = prompt("What's your name");
+console.log("How are you ", name + "?");
+
+//confirm
+var likesCats = confirm("Do you like cats?");
+if(likesCats){
+    console.log("You are a cool cat");
+}
+else{
+    console.log("Okay cool!!");
+}
+
+//Alert
+alert("Hello there!!!")
